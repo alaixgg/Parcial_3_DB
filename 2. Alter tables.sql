4b7081215231da36ee3cb3aa8210024da464d5e1
Andres Alaix <80136164+alaixgg@users.noreply.github.com>
2024-05-14 18:01:11 -0500

    Full 2. Alter tables.sql

--- 2. Alter tables.sql
+++ 2. Alter tables.sql
@@ -1,7 +1,7 @@
 ALTER TABLE NOTA_ALUMNOS
 ADD CONSTRAINT fk_id_alumno
 FOREIGN KEY (id_alumno)
-REFERENCES alumno(id_alumno);
+REFERENCES alumnos(id_alumno);
 
 ALTER TABLE ASIGNATURA
 ADD CONSTRAINT fk_id_curso
@@ -14,12 +14,12 @@ ADD CONSTRAINT pk_id_profesor
 FOREIGN KEY (id_profesor)
 REFERENCES profesor(id_profesor);
 
-ALTER TABLE ALUMNO
+ALTER TABLE ALUMNOS
 ADD CONSTRAINT pk_poblacion_alumno
 FOREIGN KEY (id_poblacion)
 REFERENCES poblacion(id_poblacion);
 
-ALTER TABLE ALUMNO
+ALTER TABLE ALUMNOS
 ADD CONSTRAINT pk_id_codigo_postal_alumno
 FOREIGN KEY (id_codigo_postal)
 REFERENCES codigo_postal(id_codigo_postal);
@@ -40,11 +40,12 @@ FOREIGN KEY (id_asignatura)
 REFERENCES asignatura(id_asignatura);
 
 ALTER TABLE CURSO
-ADD CONSTRAINT pk_id_profesor
+ADD CONSTRAINT pk_id_profesor_curso
 FOREIGN KEY (id_profesor)
 REFERENCES profesor(id_profesor);
 
-ALTER TABLE NOTA_ALUMNO
+
+ALTER TABLE NOTA_ALUMNOS
 ADD CONSTRAINT pk_id_asignatura_nota
 FOREIGN KEY (id_asignatura)
 REFERENCES asignatura(id_asignatura);
