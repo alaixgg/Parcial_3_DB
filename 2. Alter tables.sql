4c89a11f203809a1d987d742d988673a06bd4423
Andres Alaix <80136164+alaixgg@users.noreply.github.com>
2024-05-14 17:46:21 -0500

    Create 2. Alter tables.sql

---
+++ 2. Alter tables.sql
@@ -0,0 +1,50 @@
+ALTER TABLE NOTA_ALUMNOS
+ADD CONSTRAINT fk_id_alumno
+FOREIGN KEY (id_alumno)
+REFERENCES alumno(id_alumno);
+
+ALTER TABLE ASIGNATURA
+ADD CONSTRAINT fk_id_curso
+FOREIGN KEY (id_curso)
+REFERENCES curso(id_curso);
+
+
+ALTER TABLE  ASIGNATURA
+ADD CONSTRAINT pk_id_profesor
+FOREIGN KEY (id_profesor)
+REFERENCES profesor(id_profesor);
+
+ALTER TABLE ALUMNO
+ADD CONSTRAINT pk_poblacion_alumno
+FOREIGN KEY (id_poblacion)
+REFERENCES poblacion(id_poblacion);
+
+ALTER TABLE ALUMNO
+ADD CONSTRAINT pk_id_codigo_postal_alumno
+FOREIGN KEY (id_codigo_postal)
+REFERENCES codigo_postal(id_codigo_postal);
+
+ALTER TABLE PROFESOR
+ADD CONSTRAINT pk_poblacion_PROFESOR
+FOREIGN KEY (id_poblacion)
+REFERENCES poblacion(id_poblacion);
+
+ALTER TABLE PROFESOR
+ADD CONSTRAINT pk_id_codigo_postal_profesor
+FOREIGN KEY (id_codigo_postal)
+REFERENCES codigo_postal(id_codigo_postal);
+
+ALTER TABLE AULA
+ADD CONSTRAINT pk_id_asignatura
+FOREIGN KEY (id_asignatura)
+REFERENCES asignatura(id_asignatura);
+
+ALTER TABLE CURSO
+ADD CONSTRAINT pk_id_profesor
+FOREIGN KEY (id_profesor)
+REFERENCES profesor(id_profesor);
+
+ALTER TABLE NOTA_ALUMNO
+ADD CONSTRAINT pk_id_asignatura_nota
+FOREIGN KEY (id_asignatura)
+REFERENCES asignatura(id_asignatura);
